ed99765dcfc33e7d39ae905ae82154a2082d2a18
Symblox Dev <symblox@protonmail.com>
2020-12-29 22:01:18 +0800

    Added functions for building SSG pages

--- pages/pools/[networkName]/[prizePoolAddress]/home.jsx
+++ pages/pools/[networkName]/[prizePoolAddress]/home.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
 import { DepositorUI } from 'lib/components/DepositorUI'
 
+export async function getStaticProps(context) {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
+
+// This function gets called at build time
+export async function getStaticPaths() {
+  const paths = ['/pools/vlxtest/0x0AED0f913CA4A9F0Eed6Dbcb526d2DaD32ed7f03/home']
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false }
+}
+
 export default function IndexPage () {
   return <DepositorUI />
 }
--- pages/pools/[networkName]/[prizePoolAddress]/index.jsx
+++ pages/pools/[networkName]/[prizePoolAddress]/index.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import { LandingUI } from 'lib/components/LandingUI'
 
+export async function getStaticProps({params: {networkName, prizePoolAddress}}) {
+  return {
+    props: {networkName, prizePoolAddress}, // will be passed to the page component as props
+  }
+}
+
+// This function gets called at build time
+export async function getStaticPaths() {
+  const paths = [{
+    params: {
+      networkName: 'vlxtest',
+      prizePoolAddress: '0x0AED0f913CA4A9F0Eed6Dbcb526d2DaD32ed7f03'
+    }
+  }]
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false }
+}
+
 export default function IndexPage () {
   return <LandingUI />
 }
