df212ed37c813bea0606abd7dc61bbb53207c4bd
Symblox Dev <symblox@protonmail.com>
2021-01-07 11:39:38 +0800

    Show total interest in svlx on manage page

--- lib/components/ClaimRewardCard.jsx
+++ lib/components/ClaimRewardCard.jsx
@@ -1,25 +1,15 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import PrizePoolAbi from '@symblox/pvlx-contracts/abis/SyxPrizePool'
-
+import { useSvlxValues } from 'lib/hooks/useSvlxValues'
 import { sendTx } from 'lib/utils/sendTx'
-import {
-  fetchPoolChainValues,
-  poolChainValuesAtom,
-} from 'lib/hooks/usePoolChainValues'
 import { useAtom } from 'jotai'
 import { WalletContext } from 'lib/components/WalletContextProvider'
-import { useTimeLeft } from 'lib/hooks/useTimeLeft'
 import { poolAddressesAtom } from 'lib/hooks/usePoolAddresses'
-import {
-  contractVersionsAtom,
-  prizePoolTypeAtom,
-} from 'lib/hooks/useDetermineContractVersions'
-import { errorStateAtom } from 'lib/components/PoolData'
-import { Card, CardSecondaryText } from 'lib/components/Card'
+import { Card } from 'lib/components/Card'
 import { Collapse } from 'lib/components/Collapse'
 import { Button } from 'lib/components/Button'
 import { TxMessage } from 'lib/components/TxMessage'
-import { LoadingDots } from 'lib/components/LoadingDots'
+import { displayAmountInEther } from 'lib/utils/displayAmountInEther'
 
 const handleClaimInterestSubmit = async (setTx, provider, contractAddress) => {
   const params = [
@@ -72,11 +62,8 @@ export const ClaimRewardCard = () => {
 }
 
 const ClaimRewardTrigger = () => {
-  const [poolChainValues, setPoolChainValues] = useAtom(poolChainValuesAtom)
   const [poolAddresses] = useAtom(poolAddressesAtom)
-  const [prizePoolType] = useAtom(prizePoolTypeAtom)
-  const [errorState, setErrorState] = useAtom(errorStateAtom)
-  const [contractVersions] = useAtom(contractVersionsAtom)
+  const [svlxData, setSvlxData] = useSvlxValues()
   const walletContext = useContext(WalletContext)
   const provider = walletContext.state.provider
   const [tx, setTx] = useState({})
@@ -100,20 +87,6 @@ const ClaimRewardTrigger = () => {
     setTxType('Deposit Interest')
     handleDepositInterestSubmit(setTx, provider, poolAddresses.prizePool)
   }
-
-  // // If countdown has finished, trigger a chain data refetch
-  // useEffect(() => {
-  //   if (tx.completed) {
-  //     fetchPoolChainValues(
-  //       provider,
-  //       poolAddresses,
-  //       prizePoolType,
-  //       setPoolChainValues,
-  //       contractVersions.prizeStrategy.contract,
-  //       setErrorState,
-  //     )
-  //   }
-  // }, [tx.completed])
 
   if (txInFlight) {
     return (
@@ -152,6 +125,16 @@ const ClaimRewardTrigger = () => {
 
   return (
     <>
+      <div>
+        Total interest:{' '}
+        {svlxData
+          ? displayAmountInEther(svlxData.totalRewards, {
+              precision: 2,
+              decimals: 18,
+            })
+          : 0}{' '}
+        VLX
+      </div>
       <div className="flex mt-4">
         <Button
           type="button"
